0f472d30bdb6035da2a4d4f5201a30b17cb15e70
sob1rdev <20sobirjon05@gmail.com>
2024-10-18 11:39:22 +0500

    feat: add Project model and related files
    - Created Project model
    - Added migration for projects table
    - Added factory for projects
    - Created seeder for projects
    - Updated DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             StudentSeeder::class,
             LinksSeeder::class,
+            ProjectSeeder::class
         ]);
     }
 }
