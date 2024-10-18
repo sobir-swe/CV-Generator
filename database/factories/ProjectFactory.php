4a72acfff977451c46ae9ea86d8a63309cfbc087
sob1rdev <20sobirjon05@gmail.com>
2024-10-18 21:04:26 +0500

    feat: add Project model and related files
    - Created Project model
    - Added migration for projects table
    - Added factory for projects
    - Created seeder for projects
    - Updated DatabaseSeeder to include ProjectSeeder

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'source_link' => $this->faker->url(),
+            'demo_link' => $this->faker->url(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
+            ProjectSeeder::class,
         ]);
     }
 }
