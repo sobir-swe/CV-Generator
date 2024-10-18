9fda730bf0aae31ae6649b330d4e82caf3651e96
sob1rdev <20sobirjon05@gmail.com>
2024-10-18 21:35:53 +0500

    feat: add Language model and update DatabaseSeeder
    - Updated DatabaseSeeder to include LanguageSeeder
    - Created Language model
    - Added migration for languages table
    - Added factory for languages
    - Created seeder for languages

---
+++ database/factories/LanguageFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Language>
+ */
+class LanguageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'level' => $this->faker->randomElement(['A1', 'A2', 'B1', 'B2', 'C1', 'C2']),
+        ];
+    }
+}
