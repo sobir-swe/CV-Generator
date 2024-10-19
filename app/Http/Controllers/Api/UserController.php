a7df009faf260c63259dbfe75a5735c71c0102e2
sob1rdev <20sobirjon05@gmail.com>
2024-10-19 12:39:48 +0500

    Add new UserController and other new files

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): \Illuminate\Database\Eloquent\Collection
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $user = User::query()->create([
+            'first_name' => $request['first_name'],
+            'last_name' => $request['last_name'],
+            'nt_id' => $request['nt_id'],
+            'image' => $request['image'],
+            'phone' => $request['phone'],
+            'profession' => $request['profession'],
+            'biography' => $request['biography'],
+        ]);
+
+        return response()->json([
+            'message' => 'User created successfully',
+            'status' => 'success',
+            'token' => $user->createToken($user->first_name)->plainTextToken
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(User::query()->findOrFail($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): \Illuminate\Http\JsonResponse
+    {
+        $user = User::query()->findOrFail($id);
+        $user->update([
+            'first_name' => $request['first_name'],
+            'last_name' => $request['last_name'],
+            'nt_id' => $request['nt_id'],
+            'image' => $request['image'],
+            'phone' => $request['phone'],
+            'profession' => $request['profession'],
+            'biography' => $request['biography'],
+        ]);
+
+        return response()->json([
+           'message' => 'User updated successfully',
+           'status' => 'success',
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::query()->findOrFail($id);
+        $user->delete();
+        return response()->json([
+            'message' => 'User deleted successfully',
+            'status' => 'success',
+            'user' => $user
+        ]);
+    }
+}
