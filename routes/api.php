f70f5d0950ad23436b9c662d709b5cc6ab3cbb37
sob1rdev <20sobirjon05@gmail.com>
2024-10-23 09:48:11 +0500

    refactor(routes): group related API routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -10,42 +10,30 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/users', UserController::class)
-    ->middleware('auth:sanctum');
 
-Route::post('/users/{userId}/languages/attach', [LanguageController::class, 'attachLanguage'])
-    ->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::delete('/users/{userId}/languages/detach', [LanguageController::class, 'detachLanguage'])
-    ->middleware('auth:sanctum');
+    Route::resource('/users', UserController::class);
 
-Route::resource('/projects', ProjectController::class)
-    ->middleware('auth:sanctum');
+    Route::post('/users/{userId}/languages/attach', [LanguageController::class, 'attachLanguage']);
+    Route::delete('/users/{userId}/languages/detach', [LanguageController::class, 'detachLanguage']);
+    Route::post('/users/{userId}/social_networks/attach', [SocialNetworkController::class, 'attachSocialNetwork']);
+    Route::delete('/users/{userId}/social_networks/detach', [SocialNetworkController::class, 'detachSocialNetwork']);
+    Route::post('/users/{userId}/skills/attach', [SkillController::class, 'attachSkill']);
+    Route::delete('/users/{userId}/skills/detach', [SkillController::class, 'detachSkill']);
 
-Route::resource('/experiences', ExperienceController::class)
-    ->middleware('auth:sanctum');
+    Route::resource('/projects', ProjectController::class);
 
-Route::resource('/educations', EducationController::class)
-    ->middleware('auth:sanctum');
+    Route::resource('/experiences', ExperienceController::class);
 
-Route::resource('/languages', LanguageController::class)
-    ->middleware('auth:sanctum');
+    Route::resource('/educations', EducationController::class);
 
-Route::resource('/skills', SkillController::class)
-    ->middleware('auth:sanctum');
+    Route::resource('/languages', LanguageController::class);
 
-Route::post('/users/{userId}/skills/attach', [SkillController::class, 'attachSkill'])
-    ->middleware('auth:sanctum');
+    Route::resource('/skills', SkillController::class);
 
-Route::delete('/users/{userId}/skills/detach', [SkillController::class, 'detachSkill'])
-    ->middleware('auth:sanctum');
+    Route::resource('/social_networks', SocialNetworkController::class);
 
-Route::resource('/social_networks', SocialNetworkController::class)
-    ->middleware('auth:sanctum');
+});
 
-Route::post('/users/{userId}/social_networks/attach', [SocialNetworkController::class, 'attachSocialNetwork'])
-    ->middleware('auth:sanctum');
-
-Route::delete('/users/{userId}/social_networks/detach', [SocialNetworkController::class, 'detachSocialNetwork'])
-    ->middleware('auth:sanctum');
 
